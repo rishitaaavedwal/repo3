e06e259f0d27c0e972cd0f14059880b259982eb1
rishitaaavedwal <rishita.vedwal@s.amity.edu>
2025-05-24 21:06:51 +0530

    C4 in yooo

--- per.c
+++ per.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdbool.h>
 int main()
 {
-	int n=28, sum=1, i=2;
-	while (i*i<n)
+	int n=28, sum=1;
+	for (int i=2; i<=n/2; i++)
 	{
 		if (n%i==0)
 		{
 			sum+=i;
-			if(i*i!=n)
-			{
-				sum+=n/i;
-			}
 		}
-		i++;
 	}
 	bool per=(n>1 && sum==n);
 	if (per)
