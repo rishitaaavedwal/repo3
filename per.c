bee86219bcfd075a01537c2f53eddbb8b0eb1161
rishitaaavedwal <rishita.vedwal@s.amity.edu>
2025-05-24 21:20:39 +0530

    C9 in master

--- per.c
+++ per.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 int main()
 {
 	int n=28, sum=1;
-	for (int i=2; i<n; i++)
+	bool per=false;
+	for (int i=2; i*i<=n; i++)
 	{
 		if (n%i==0)
 		{
 			sum+=i;
+			if (i*i!=n)
+			{
+				sum+=n/i;
+			}
+		}
+		if(sum>n)
+		{
+			break;
 		}
 	}
-	bool per=(n>1 && sum==n);
+	if (n>1 && sum==n) per=true;
 	if (per)
 	{
 		printf("perfect no");
