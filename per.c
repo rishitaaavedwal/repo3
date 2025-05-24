1141075bd7d510caeda0ef316c071e604d65f1fa
rishitaaavedwal <rishita.vedwal@s.amity.edu>
2025-05-24 21:31:21 +0530

    C12 in yoooooooo

--- per.c
+++ per.c
@@ -4,15 +4,11 @@
 int main()
 {
 	int n=28, sum=1;
-	for (int i=1; i<sqrt(n)+1; i++)
+	for (int i=2; i<n; i++)
 	{
-		if (i>n && n%i==0)
+		if (n%i==0)
 		{
 			sum+=i;
-			if (i*i!=n && n/i=n)
-			{
-				sum+=n/i;
-			}
 		}
 	}
 	bool per=(n>1 && sum==n);
