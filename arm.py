448c1e6e558147ff733a917a273a6caa293cffbf
rishitaaavedwal <rishita.vedwal@s.amity.edu>
2025-05-28 21:22:48 +0530

    C5 in qwwww

--- arm.py
+++ arm.py
@@ -1,9 +1,6 @@
-def armstrong_recursive(num, n):
-    if num == 0:
-        return 0
-    digit = num % 10
-    return digit ** n + armstrong_recursive(num // 10, n)
-
 num = int(input("Enter a number: "))
-n = len(str(num))
-print("Armstrong number" if armstrong_recursive(num, n) == num else "Not an Armstrong number")
+digits = list(map(int, str(num)))
+n = len(digits)
+total = sum(map(lambda x: x ** n, digits))
+
+print("Armstrong number" if total == num else "Not an Armstrong number")
